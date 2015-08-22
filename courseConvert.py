20a068916195118885bc288a94c928772e693634
jason0342 <jasonwang@JasondeMacBook-Pro.local>
2015-08-22 21:14:00 +0800

    timeToInt func

--- courseConvert.py
+++ courseConvert.py
@@ -9,6 +9,7 @@ class course:
 		self.c_time = ti
 		self.c_type = ty
 		self.int_time = []
+
 	def set(self, no, tit, de, ti, ty):
 		self.c_no = no
 		self.c_title = tit
@@ -16,11 +17,28 @@ class course:
 		self.c_time = ti
 		self.c_type = ty
 
+	def timeToInt(self):
+		week = ["一","二","三","四","五","六"]
+		order = ["A", "B", "C", "D"]
+		index = []
+		for day in week:
+			if day in self.c_time:
+				index.append(self.c_time.index(day))
+		index.append(len(self.c_time)+1)
+		for i in range(len(index)-1):
+			temp = self.c_time[index[i]+1: index[i+1]]
+			for j in temp.strip().split(","):
+				if j.isdecimal():
+					self.int_time.append(week.index(self.c_time[index[i]])*15 + int(j) + 1)
+				elif j in order:
+					self.int_time.append(week.index(self.c_time[index[i]])*15 + order.index(j) + 12)
+
+
 def convertTime(c_time):
 	if c_time == "":
 		return "0"
 	count = c_time.count("(")
-	out = ""
+	out = "" 
 	for i in range(count):
 		if "(" in c_time:
 			lindex = c_time.index("(")
@@ -28,7 +46,7 @@ def convertTime(c_time):
 			temp = c_time.partition(c_time[lindex: rindex+1])
 			out += temp[0]
 			c_time = temp[2]
-	out.replace(" ", "")
+	out = out.replace(" ", "")
 	return out
 
 
@@ -46,21 +64,7 @@ def convertType(c_type):
 					return c_type[index+1]
 	return "0"
 
-def timeToInt(c_time):
-	week = ["一","二","三","四","五","六"]
-	order = ["A", "B", "C", "D"]
-	index = []
-	for day in week:
-		if day in c_time:
-			index.append(c_time.index(day))
-	index.append(len(c_time)+1)
-	for i in range(len(index)-1):
-		temp = c_time[index[i]+1: index[i+1]]
-		for j in temp.strip().split(","):
-			if j.isdecimal():
-				int_time.append(week.index(c_time[index[i]])*15 + int(j))
-			else if j in order:
-				int_time.append(week.index(c_time[index[i]])*15 + order.index(j) + 12)
+
 
 
 
@@ -78,14 +82,14 @@ while i + 15 <= len(data):
 	if data[i+1] == "":
 		data[i+1] = "0"
 	course_list.append(course(data[i], data[i+4], data[i+1], convertTime(data[i+11]), convertType(data[i+14])))
-	print(data[i])
+	# print(data[i])
 	i += 15
 
-# output = codecs.open("output.txt", "w", "utf-8")
-# for c in course_list:
-# 	output.write(c.c_no + " " + c.c_dep + " " + c.c_time + " " + c.c_type + "\n")
-
+output = codecs.open("output.txt", "w", "utf-8")
 for c in course_list:
+	c.timeToInt()
+	output.write(c.c_no + " " + c.c_dep + " " + str(len(c.int_time)) + " " + " ".join(str(x) for x in c.int_time) + " " + c.c_type + "\n")
+
 
 
 
