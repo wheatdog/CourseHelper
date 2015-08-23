2b2e9992254fec1347c232c8ea8277e089ed6a0e
Bryce Tsao <tsaodingtw@gmail.com>
2015-08-23 08:43:29 +0800

    fixed print

--- parsing.py
+++ parsing.py
@@ -9,7 +9,7 @@ PageCount = 0
 PageMax = 603
 # PageMax = 2
 while PageCount < PageMax:
-    print PageCount
+    print (PageCount)
 
     Content = urllib2.urlopen(URL + str(PageCount)).read()
     Soup = BeautifulSoup(Content, 'html')
